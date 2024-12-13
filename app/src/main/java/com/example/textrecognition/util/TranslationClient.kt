d94612e65d438d59f00de3b840d1c5cadf53acc3
kaaneneskpc <kaaneneskpc1@gmail.com>
2024-12-13 08:35:46 +0300

    - Create TranslationClient object for common usages.

--- app/src/main/java/com/example/textrecognition/util/TranslationClient.kt
+++ app/src/main/java/com/example/textrecognition/util/TranslationClient.kt
@@ -1,13 +1,13 @@
 package com.example.textrecognition.util
 
-    import com.google.mlkit.nl.translate.TranslateLanguage
-    import com.google.mlkit.nl.translate.Translation
-    import com.google.mlkit.nl.translate.TranslatorOptions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
-    object TranslationClient {
-        val options = TranslatorOptions.Builder()
-            .setSourceLanguage(TranslateLanguage.ENGLISH)
-            .setTargetLanguage(TranslateLanguage.TURKISH)
-            .build()
-        val translator = Translation.getClient(options)
-    }
+object TranslationClient {
+    val options = TranslatorOptions.Builder()
+        .setSourceLanguage(TranslateLanguage.ENGLISH)
+        .setTargetLanguage(TranslateLanguage.TURKISH)
+        .build()
+    val translator = Translation.getClient(options)
+}
